2699ad80fbf2afe3fd64b9ab3b74a14dfd8505ed
Pierre-Henry Soria <hizup.com@hotmail.com>
2013-02-22 16:43:57 -0500

    Security, We check whether this is a url and if the website is up ...

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,4 @@
 class Url < ActiveRecord::Base
   attr_accessible :url
-  validates :url, :presence => true, :uniqueness => true, :length => { :minimum => 10, :maximum => 150 }
+  validates :url, :presence => true, :uniqueness => true, :length => { :minimum => 10, :maximum => 150 } , :uri => { :format => /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix }
 end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
+require File.expand_path('../../lib/validates/url', __FILE__) # Validate URLs in Models
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
